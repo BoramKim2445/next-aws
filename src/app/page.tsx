a802dfabc5d770576e9577e40a5ee1edf1fdb444
Boram Kim <boram@goyoai.com>
2024-02-09 17:18:20 +0900

    Feat: cypress setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <main>
-      <h1 className='text-5xl m-5'>Hello Next.js!!!🥳</h1>
+      <h1 className='text-5xl m-5'>HomePage</h1>
+      <h2 className='text-xl m-5'>Hello Next.js!!!🥳</h2>
+      <Link href='/about' className='hover:underline'>
+        About
+      </Link>
     </main>
   );
 }
